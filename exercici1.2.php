210edf09084ecf5e9c90b0c8edda8cda8ee113b0
Isaac <ifuriomartin.cf@iesesteveterradas.cat>
2023-09-20 04:12:22 +0200

    No adjacent ships implemented

--- exercici1.2.php
+++ exercici1.2.php
@@ -53,7 +53,7 @@
             echo "  <div class=\"autocenter\">";
             echo "  <h1>Batalla Naval</h1>";
 
-            $boats = [1, 2 ,3 ,4]; // fragata, submarino, destructor, portaviones
+            // fragata = 1, submarino = 2, destructor = 3, portaviones = 4
 
             $colums = 11;
             $rows = 11;
@@ -69,47 +69,96 @@
                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
-            // 4 fragates, 3 submarins, 2 destructors i 1 portaavions.
+            function checkAdjacent($fila, $columna) {
+                // logic of adjacent and matrix limits:
+                // checkAdjacent checks if there are any adjacent ship in the current position
+                // also if the adjacent position is inside the matrix (using isset)
+                global $matrix;
+                if (isset($matrix[$fila - 1][$columna])) {
+                    if ($matrix[$fila - 1][$columna] != 0) {
+                        return false;
+                    }
+                }
+                if (isset($matrix[$fila + 1][$columna])) {
+                    if ($matrix[$fila + 1][$columna] != 0) {
+                        return false;
+                    }
+                }
+                if (isset($matrix[$fila][$columna - 1])) {
+                    if ($matrix[$fila][$columna - 1] != 0) {
+                        return false;
+                    }
+                }
+                if (isset($matrix[$fila][$columna + 1])) {
+                    if ($matrix[$fila][$columna + 1] != 0) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            // 4 frigates, 3 submarines, 2 destroyers i 1 carrier.
+
             $totalBoats = 10;
             for ($i = 1; $i <= $totalBoats;) {
                 $rowPosition = rand(0, 9);
                 $columPosition = rand(0, 9);
                 $orientation = rand(0, 1);
+
                 if ($i <= 4) { // set frigate position
                     if ($matrix[$rowPosition][$columPosition] == 0) {
-                        $matrix[$rowPosition][$columPosition] = 1;
-                        $i++;
+                        $result = checkAdjacent($rowPosition, $columPosition);
+                        if ($result) {   // set frigate position
+                            $matrix[$rowPosition][$columPosition] = 1;
+                            $i++;
+                        }     
                     }
                 }
                 elseif ($i <= 7) { // set submarine position
                     if ($orientation == 0) { // horizontal
                         if ($columPosition == $colums - 2) { // this is the final column
                             if ($matrix[$rowPosition][$columPosition] == 0 && $matrix[$rowPosition][$columPosition - 1] == 0) {
-                                $matrix[$rowPosition][$columPosition] = 2;
-                                $matrix[$rowPosition][$columPosition - 1] = 2;
-                                $i++;
+                                $result = checkAdjacent($rowPosition, $columPosition);
+                                $result2 = checkAdjacent($rowPosition, $columPosition - 1);
+                                if ($result && $result2) {
+                                    $matrix[$rowPosition][$columPosition] = 2;
+                                    $matrix[$rowPosition][$columPosition - 1] = 2;
+                                    $i++;
+                                }
                             }
                         }
                         else { // the column can be the first or else
                             if ($matrix[$rowPosition][$columPosition] == 0 && $matrix[$rowPosition][$columPosition + 1] == 0) {
-                                $matrix[$rowPosition][$columPosition] = 2;
-                                $matrix[$rowPosition][$columPosition + 1] = 2;
-                                $i++;
+                                $result = checkAdjacent($rowPosition, $columPosition);
+                                $result2 = checkAdjacent($rowPosition, $columPosition + 1);
+                                if ($result && $result2) {
+                                    $matrix[$rowPosition][$columPosition] = 2;
+                                    $matrix[$rowPosition][$columPosition + 1] = 2;
+                                    $i++;
+                                }
                             }
                         }
                     } else { // vertical
                         if ($rowPosition == $rows - 2) { // this is the final row
                             if ($matrix[$rowPosition][$columPosition] == 0 && $matrix[$rowPosition - 1][$columPosition] == 0) {
-                                $matrix[$rowPosition][$columPosition] = 2;
-                                $matrix[$rowPosition - 1][$columPosition] = 2;
-                                $i++;
+                                $result = checkAdjacent($rowPosition, $columPosition);
+                                $result2 = checkAdjacent($rowPosition - 1, $columPosition);
+                                if ($result && $result2) {
+                                    $matrix[$rowPosition][$columPosition] = 2;
+                                    $matrix[$rowPosition - 1][$columPosition] = 2;
+                                    $i++;
+                                }
                             }
                         }
                         else { // the row can be the first or else
                             if ($matrix[$rowPosition][$columPosition] == 0 && $matrix[$rowPosition + 1][$columPosition] == 0) {
-                                $matrix[$rowPosition][$columPosition] = 2;
-                                $matrix[$rowPosition + 1][$columPosition] = 2;
-                                $i++;
+                                $result = checkAdjacent($rowPosition, $columPosition);
+                                $result2 = checkAdjacent($rowPosition + 1, $columPosition);
+                                if ($result && $result2) {
+                                    $matrix[$rowPosition][$columPosition] = 2;
+                                    $matrix[$rowPosition + 1][$columPosition] = 2;
+                                    $i++;
+                                } 
                             }
                         }
                     }
@@ -118,34 +167,54 @@
                     if ($orientation == 0) { // horizontal
                         if ($columPosition + 3 > $colums - 2) { // it have to go to left
                             if ($matrix[$rowPosition][$columPosition] == 0 && $matrix[$rowPosition][$columPosition - 1] == 0 && $matrix[$rowPosition][$columPosition - 2] == 0) {
-                                $matrix[$rowPosition][$columPosition] = 3;
-                                $matrix[$rowPosition][$columPosition - 1] = 3;
-                                $matrix[$rowPosition][$columPosition - 2] = 3;
-                                $i++;
+                                $result = checkAdjacent($rowPosition, $columPosition);
+                                $result2 = checkAdjacent($rowPosition, $columPosition - 1);
+                                $result3 = checkAdjacent($rowPosition, $columPosition - 2);
+                                if ($result && $result2 && $result3) {
+                                    $matrix[$rowPosition][$columPosition] = 3;
+                                    $matrix[$rowPosition][$columPosition - 1] = 3;
+                                    $matrix[$rowPosition][$columPosition - 2] = 3;
+                                    $i++;
+                                }
                             }
                         } else { // always go to right
                             if ($matrix[$rowPosition][$columPosition] == 0 && $matrix[$rowPosition][$columPosition + 1] == 0 && $matrix[$rowPosition][$columPosition + 2] == 0) {
-                                $matrix[$rowPosition][$columPosition] = 3;
-                                $matrix[$rowPosition][$columPosition + 1] = 3;
-                                $matrix[$rowPosition][$columPosition + 2] = 3;
-                                $i++;
+                                $result = checkAdjacent($rowPosition, $columPosition);
+                                $result2 = checkAdjacent($rowPosition, $columPosition + 1);
+                                $result3 = checkAdjacent($rowPosition, $columPosition + 2);
+                                if ($result && $result2 && $result3) {
+                                    $matrix[$rowPosition][$columPosition] = 3;
+                                    $matrix[$rowPosition][$columPosition + 1] = 3;
+                                    $matrix[$rowPosition][$columPosition + 2] = 3;
+                                    $i++;
+                                }
                             }
                         }
                     }
                     else { // vertical
                         if ($rowPosition + 3 > $rows - 2) { // it have to go up
                             if ($matrix[$rowPosition][$columPosition] == 0 && $matrix[$rowPosition - 1][$columPosition] == 0 && $matrix[$rowPosition - 2][$columPosition] == 0) {
-                                $matrix[$rowPosition][$columPosition] = 3;
-                                $matrix[$rowPosition - 1][$columPosition] = 3;
-                                $matrix[$rowPosition - 2][$columPosition] = 3;
-                                $i++;
+                                $result = checkAdjacent($rowPosition, $columPosition);
+                                $result2 = checkAdjacent($rowPosition - 1, $columPosition);
+                                $result3 = checkAdjacent($rowPosition - 2, $columPosition);
+                                if ($result && $result2 && $result3) {
+                                    $matrix[$rowPosition][$columPosition] = 3;
+                                    $matrix[$rowPosition - 1][$columPosition] = 3;
+                                    $matrix[$rowPosition - 2][$columPosition] = 3;
+                                    $i++;
+                                }
                             }
                         } else { // always go down
                             if ($matrix[$rowPosition][$columPosition] == 0 && $matrix[$rowPosition + 1][$columPosition] == 0 && $matrix[$rowPosition + 2][$columPosition] == 0) {
-                                $matrix[$rowPosition][$columPosition] = 3;
-                                $matrix[$rowPosition + 1][$columPosition] = 3;
-                                $matrix[$rowPosition + 2][$columPosition] = 3;
-                                $i++;
+                                $result = checkAdjacent($rowPosition, $columPosition);
+                                $result2 = checkAdjacent($rowPosition + 1, $columPosition);
+                                $result3 = checkAdjacent($rowPosition + 2, $columPosition);
+                                if ($result && $result2 && $result3) {
+                                    $matrix[$rowPosition][$columPosition] = 3;
+                                    $matrix[$rowPosition + 1][$columPosition] = 3;
+                                    $matrix[$rowPosition + 2][$columPosition] = 3;
+                                    $i++;
+                                }
                             }
                         }
                     }
@@ -154,44 +223,64 @@
                         if ($columPosition + 4 > $colums - 2) { // it have to go to left
                             if ($matrix[$rowPosition][$columPosition] == 0 && $matrix[$rowPosition][$columPosition - 1] == 0 && $matrix[$rowPosition][$columPosition - 2] == 0 && 
                                 $matrix[$rowPosition][$columPosition - 3] == 0) {
-
-                                $matrix[$rowPosition][$columPosition] = 4;
-                                $matrix[$rowPosition][$columPosition - 1] = 4;
-                                $matrix[$rowPosition][$columPosition - 2] = 4;
-                                $matrix[$rowPosition][$columPosition - 3] = 4;
-                                $i++;
+                                $result = checkAdjacent($rowPosition, $columPosition);
+                                $result2 = checkAdjacent($rowPosition, $columPosition - 1);
+                                $result3 = checkAdjacent($rowPosition, $columPosition - 2);
+                                $result4 = checkAdjacent($rowPosition, $columPosition - 3);
+                                if ($result && $result2 && $result3 && $result4) {
+                                    $matrix[$rowPosition][$columPosition] = 4;
+                                    $matrix[$rowPosition][$columPosition - 1] = 4;
+                                    $matrix[$rowPosition][$columPosition - 2] = 4;
+                                    $matrix[$rowPosition][$columPosition - 3] = 4;
+                                    $i++;
+                                }
                             }
                         } else { // always go to the right
                             if ($matrix[$rowPosition][$columPosition] == 0 && $matrix[$rowPosition][$columPosition + 1] == 0 && $matrix[$rowPosition][$columPosition + 2] == 0 && 
                                 $matrix[$rowPosition][$columPosition + 3] == 0) {
-                                
-                                $matrix[$rowPosition][$columPosition] = 4;
-                                $matrix[$rowPosition][$columPosition + 1] = 4;
-                                $matrix[$rowPosition][$columPosition + 2] = 4;
-                                $matrix[$rowPosition][$columPosition + 3] = 4;
-                                $i++;
+                                $result = checkAdjacent($rowPosition, $columPosition);
+                                $result2 = checkAdjacent($rowPosition, $columPosition + 1);
+                                $result3 = checkAdjacent($rowPosition, $columPosition + 2);
+                                $result4 = checkAdjacent($rowPosition, $columPosition + 3);
+                                if ($result && $result2 && $result3 && $result4) {
+                                    $matrix[$rowPosition][$columPosition] = 4;
+                                    $matrix[$rowPosition][$columPosition + 1] = 4;
+                                    $matrix[$rowPosition][$columPosition + 2] = 4;
+                                    $matrix[$rowPosition][$columPosition + 3] = 4;
+                                    $i++;
+                                }
                             }
                         }
                     } else { // vertical 
                         if ($rowPosition + 4 > $rows - 2) { // it have to go to up
                             if ($matrix[$rowPosition][$columPosition] == 0 && $matrix[$rowPosition - 1][$columPosition] == 0 && $matrix[$rowPosition - 2][$columPosition] == 0 && 
                                 $matrix[$rowPosition - 3][$columPosition] == 0) {
-                                
-                                $matrix[$rowPosition][$columPosition] = 4;
-                                $matrix[$rowPosition - 1][$columPosition] = 4;
-                                $matrix[$rowPosition - 2][$columPosition] = 4;
-                                $matrix[$rowPosition - 3][$columPosition] = 4;
-                                $i++;
+                                $result = checkAdjacent($rowPosition, $columPosition);
+                                $result2 = checkAdjacent($rowPosition - 1, $columPosition);
+                                $result3 = checkAdjacent($rowPosition - 2, $columPosition);
+                                $result4 = checkAdjacent($rowPosition - 3, $columPosition);
+                                if ($result && $result2 && $result3 && $result4) {
+                                    $matrix[$rowPosition][$columPosition] = 4;
+                                    $matrix[$rowPosition - 1][$columPosition] = 4;
+                                    $matrix[$rowPosition - 2][$columPosition] = 4;
+                                    $matrix[$rowPosition - 3][$columPosition] = 4;
+                                    $i++;
+                                }
                             }
                         } else { // always go down
                             if ($matrix[$rowPosition][$columPosition] == 0 && $matrix[$rowPosition + 1][$columPosition] == 0 && $matrix[$rowPosition + 2][$columPosition] == 0 && 
                                 $matrix[$rowPosition + 3][$columPosition] == 0) {
-                                
-                                $matrix[$rowPosition][$columPosition] = 4;
-                                $matrix[$rowPosition + 1][$columPosition] = 4;
-                                $matrix[$rowPosition + 2][$columPosition] = 4;
-                                $matrix[$rowPosition + 3][$columPosition] = 4;
-                                $i++;
+                                $result = checkAdjacent($rowPosition, $columPosition);
+                                $result2 = checkAdjacent($rowPosition + 1, $columPosition);
+                                $result3 = checkAdjacent($rowPosition + 2, $columPosition);
+                                $result4 = checkAdjacent($rowPosition + 3, $columPosition);
+                                if ($result && $result2 && $result3 && $result4) {
+                                    $matrix[$rowPosition][$columPosition] = 4;
+                                    $matrix[$rowPosition + 1][$columPosition] = 4;
+                                    $matrix[$rowPosition + 2][$columPosition] = 4;
+                                    $matrix[$rowPosition + 3][$columPosition] = 4;
+                                    $i++;
+                                }  
                             }
                         }
                     }
